7b5390a56260688af2d7d2a65eec8c44e62d06cf
Simon Piercy <Simon.Piercy@ctrmcubed.com>
2021-10-08 17:22:41 +0100

    Added Redis logging.

--- src/Fidectus/Services/FidectusAuthenticationService.cs
+++ src/Fidectus/Services/FidectusAuthenticationService.cs
@@ -19,11 +19,13 @@ namespace Fidectus.Services
 
         private readonly IHttpClientFactory httpClientFactory;
         private readonly IRedisService redisService;
+        private readonly ILogger<ApiService> logger;
 
         public FidectusAuthenticationService(IHttpClientFactory httpClientFactory, IRedisService redisService, ILogger<ApiService> logger) : base(logger)
         {
             this.httpClientFactory = httpClientFactory;
             this.redisService = redisService;
+            this.logger = logger;
         }
 
         public async Task<RequestTokenResponse> FidectusGetAuthenticationToken(RequestTokenRequest requestTokenRequest, IFidectusConfiguration fidectusConfiguration)
@@ -32,12 +34,16 @@ namespace Fidectus.Services
 
             if (!string.IsNullOrWhiteSpace(requestTokenResponse?.AccessToken))
             {
+                logger.LogInformation("Reusing cached Fidectus access token...");
+
                 return new RequestTokenResponse
                 {
                     AccessToken = requestTokenResponse.AccessToken,
                     IsSuccessStatusCode = true
                 };
             }
+
+            logger.LogInformation("Getting new Fidectus access token...");
 
             var httpClient = httpClientFactory.CreateClient();
 
@@ -47,6 +53,8 @@ namespace Fidectus.Services
             var fidectusAuthenticationToken = new FidectusAuthenticationToken { AccessToken = getAuthenticationToken.AccessToken };
 
             await redisService.Set(fidectusAuthenticationToken, 24, RedisConstants.FidectusAuthenticationTokenKey, requestTokenRequest.ClientId);
+
+            logger.LogInformation("New Fidectus access token cached");
 
             return getAuthenticationToken;
         }
