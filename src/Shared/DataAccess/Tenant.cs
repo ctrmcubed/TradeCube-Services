e77a2e6b12b024b234bb7f81d6cdb2433c00ba73
Andy Bennett <Andy.Bennett@ctrmcubed.com>
2023-03-09 10:45:09 +0000

    Added MongoDb logging.

---
+++ src/Shared/DataAccess/Tenant.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.DataAccess;
+
+public class Tenant
+{
+    public string ScafellConnectionString { get; }
+    
+    public Tenant()
+    {
+        ScafellConnectionString = System.Environment.GetEnvironmentVariable("SCAFELL_SCAFELL_CONNECTION_STRING");
+    }
+}
--- src/TradeCube-Services/Extensions/LoggingExtensions.cs
+++ src/TradeCube-Services/Extensions/LoggingExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Serilog;
 using Serilog.Core;
+using Shared.DataAccess;
 using ILogger = Serilog.ILogger;
 
 namespace TradeCube_Services.Extensions;
@@ -17,21 +18,43 @@ public static class LoggingExtensions
 
     public static ILogger CreateLogger(this LoggerConfiguration loggerConfiguration, string fileName, IConfigurationRoot configuration)
     {
-        var file = loggerConfiguration
-            .ReadFrom.Configuration(configuration)
-            .Enrich.FromLogContext()
-            .Enrich.WithMachineName()
-            .Enrich.WithEnvironmentName()
-            .WriteTo.Console()
-            .WriteTo.File(fileName, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 10);
-
-        var logStashUriEnvVar = Environment.GetEnvironmentVariable("SCAFELL_LOGSTASH_URI") ?? string.Empty;
-
-        if (!string.IsNullOrWhiteSpace(logStashUriEnvVar))
+        try
         {
-            file.WriteTo.Http(requestUri: new Uri(logStashUriEnvVar).ToString(), queueLimitBytes: null);
-        }
+            var file = loggerConfiguration
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithEnvironmentName()
+                .WriteTo.Console()
+                .WriteTo.File(fileName, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 10);
 
-        return file.CreateLogger();
+            var logStashUriEnvVar = Environment.GetEnvironmentVariable("SCAFELL_LOGSTASH_URI") ?? string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(logStashUriEnvVar))
+            {
+                file.WriteTo.Http(requestUri: new Uri(logStashUriEnvVar).ToString(), queueLimitBytes: null);
+            }
+
+            var loggingCollection = Environment.GetEnvironmentVariable("SCAFELL_LOGGING_COLLECTION");
+
+            if (string.IsNullOrWhiteSpace(loggingCollection))
+            {
+                return file.CreateLogger();
+            }
+
+            file.WriteTo.MongoDBBson(cfg =>
+            {
+                cfg.SetMongoUrl(new Tenant().ScafellConnectionString);
+                cfg.SetCollectionName(loggingCollection);
+                cfg.SetBatchPeriod(TimeSpan.FromSeconds(2));
+                cfg.SetExpireTTL(TimeSpan.FromDays(90));
+            });
+
+            return file.CreateLogger();
+        }
+        catch (Exception)
+        {
+            return CreateSimpleLogger(loggerConfiguration, fileName);
+        }
     }
 }
